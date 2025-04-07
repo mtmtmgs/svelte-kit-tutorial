8babf255f7a01864a214b4a1dd42701df1c65dea
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2025-04-07 16:31:27 +0900

    feat(auth): add GitHub and Facebook providers, enhance login UI
    - Integrated GitHub and Facebook authentication providers.
    - Improved login page layout with responsive design.
    - Updated sign-in and sign-out button styles for better user experience.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,16 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/core/providers/google';
+import GitHub from '@auth/core/providers/github';
+import Facebook from '@auth/core/providers/facebook';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+    }),
+    GitHub({}),
+    Facebook({})
   ],
   secret: process.env.AUTH_SECRET
 });
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,26 +1,40 @@
 <script>
   import { SignIn, SignOut } from '@auth/sveltekit/components';
   import { page } from '$app/stores';
+  import { Button, ButtonGroup } from 'flowbite-svelte';
 </script>
 
-<h1>SvelteKit Auth Example</h1>
-<div>
+<h1 class="m-5">SvelteKit Auth Example</h1>
+<div class="flex h-screen flex-col items-center justify-center">
   {#if $page.data.session}
     {#if $page.data.session.user?.image}
       <img src={$page.data.session.user.image} class="avatar" alt="User Avatar" />
     {/if}
-    <span class="signedInText">
+    <div class="m-3 text-green-500">
       <small>Signed in as</small><br />
       <strong>{$page.data.session.user?.name ?? 'User'}</strong>
-    </span>
+    </div>
     <SignOut>
       <div slot="submitButton" class="buttonPrimary">Sign out</div>
     </SignOut>
   {:else}
-    <span class="notSignedInText">You are not signed in</span>
-    <SignIn>
-      <div slot="submitButton" class="buttonPrimary">Sign in</div>
-    </SignIn>
-    <SignIn provider="google" />
+    <div class="m-3 text-red-500">You are not signed in</div>
+    <ButtonGroup
+      class="mb-3 flex flex-col items-center"
+      aria-label="Basic example"
+      data-tooltip-target="tooltip-default"
+      data-tooltip-placement="top"
+      data-tooltip-trigger="hover"
+      data-tooltip-content="Sign in with your email and password"
+    >
+      <Button class="mb-2">
+        <SignIn>
+          <div slot="submitButton">Sign in</div>
+        </SignIn>
+      </Button>
+      <Button>
+        <SignIn provider="google" />
+      </Button>
+    </ButtonGroup>
   {/if}
 </div>
