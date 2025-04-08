349e689ea53fe75b07fb27cc945ed478153c3f6a
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2025-04-08 17:58:32 +0900

    fix(auth): change unauthorized response from redirect to JSON
    - Updated the authorization handle to return a JSON response with a 401 status for unauthorized access instead of redirecting to the login page.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
-import { redirect, type Handle } from '@sveltejs/kit';
-import { handle as authenticationHandle } from './lib/auth';
+import { json, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { handle as authenticationHandle } from './lib/auth';
 
 async function authorizationHandle({ event, resolve }) {
   if (event.url.pathname.startsWith('/api')) {
     const session = await event.locals.auth();
     if (!session) {
-      throw redirect(303, '/login');
+      return json({ message: 'Unauthorized' }, { status: 401 });
     }
   }
 
