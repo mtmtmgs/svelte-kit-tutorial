25f6ee389b677139d3d4d93cfcc38d279b8cee12
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2025-04-26 03:13:03 +0900

    slot

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,15 @@
+<!-- lib/Card.svelte -->
+<div class="rounded border p-4 shadow">
+  <header class="text-lg font-bold text-blue-500">
+    <slot name="title" />
+  </header>
+
+  <main class="my-5">
+    <slot />
+    <!-- 無名スロット -->
+  </main>
+
+  <footer class="text-sm text-gray-400">
+    <slot name="footer" />
+  </footer>
+</div>
---
+++ src/routes/slot/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Card from '$lib/components/Card.svelte';
+</script>
+
+<Card>
+  <div slot="title">✨ スロットタイトル ✨</div>
+
+  <p>これは本文です。無名スロットに入ります1。</p>
+  <p>これは本文です。無名スロットに入ります2。</p>
+
+  <div slot="footer">© フッターコンテンツ</div>
+</Card>
